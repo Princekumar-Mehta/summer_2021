989ad436f3b387a15817f4aadaa2d427b682d490
Princekumar-Mehta <62256420+Princekumar-Mehta@users.noreply.github.com>
2021-07-22 23:15:55 +0530

    Delete source_code directory

--- source_code/pdo.php
+++
@@ -1,5 +0,0 @@
-<?php
-$pdo = new PDO('mysql:host=localhost;port=3306;dbname=project_db',
-   'Prince', 'p@2001');
-// See the "errors" folder for details...
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
